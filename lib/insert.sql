c375cfd8fea56269a5898cd7ccc73ace8eb0a1cd
Will Lovell <willard.lovell@icloud.com>
2022-10-15 22:06:10 -0600

    Completed Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ('Mr. Chocolate', 5, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Rowdy', 3, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Tabitha', 2, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Sergeant Brown', 10, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Melissa', 5, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Grinch', 7, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Wendy', 1, 'F');
+INSERT INTO bears (name, age, sex) VALUES (NULL, 5, 'M');
